16ac253f5e86bc94bb43506033544b68b3c64ddb
pkcoder1 <133606175+pkcoder1@users.noreply.github.com>
2024-01-08 13:20:42 -0500

    Update ValidAnagram.py

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,12 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s)!=len(t):
+            return False 
 
-        one = list(s)
-        two = list(t)
+        counts = {}
+        countt = {}
 
-        one.sort()
-        two.sort()
+        for i in range(len(s)):
+            if s[i] not in counts:
+                counts[s[i]]=0
+            else:
+                counts[s[i]]+=1
+        print(counts)
+        for i in range(len(t)):
+            if t[i] not in countt:
+                countt[t[i]]=0
+            else:
+                countt[t[i]]+=1
+           
+                
 
-        if one != two:
-            return False
-        return True
+        if counts==countt:
+            return True 
+        
+        return False 
